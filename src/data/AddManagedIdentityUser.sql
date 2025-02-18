1392cf625665e1f632ea00e9b6abe3539af893ac
hana-farid <hana.farid@exeon.com>
2025-02-18 16:51:13 +0000

    Exercise 2.1

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER [id-awl2dpwuab7eo] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [id-awl2dpwuab7eo];
+ALTER ROLE db_datawriter ADD MEMBER [id-awl2dpwuab7eo];
 
